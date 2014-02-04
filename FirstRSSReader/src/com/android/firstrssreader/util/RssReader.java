6c86635fb1ffc705d30a378acae308530d8d36b2
wXc <wXc@algernons-mbp>
2014-02-03 21:51:03 -0500

    RssReader Class added.

--- FirstRSSReader/src/com/android/firstrssreader/util/RssReader.java
+++ FirstRSSReader/src/com/android/firstrssreader/util/RssReader.java
@@ -9,4 +9,18 @@ import com.android.firstrssreader.data.RssItem;
 
 public class RssReader {
 	
+	private String rssUrl;
+	
+	public RssReader(String rssUrl) {
+		this.rssUrl = rssUrl;
+	}
+	
+	public List<RssItem> getItems() throws Exception {
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = factory.newSAXParser();
+		
+		RssParseHandler handler = new RssParseHandler();
+		saxParser.parse(rssUrl, handler);
+		return handler.getItems();
+	}
 }
