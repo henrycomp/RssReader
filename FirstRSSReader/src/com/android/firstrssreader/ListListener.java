58272ad3d93270d2ade766d8fb0bddb74b83a287
wXc <wXc@algernons-mbp>
2014-02-06 21:52:23 -0500

    Started ListListener class

--- FirstRSSReader/src/com/android/firstrssreader/ListListener.java
+++ FirstRSSReader/src/com/android/firstrssreader/ListListener.java
@@ -2,9 +2,23 @@ package com.android.firstrssreader;
 
 import java.util.List;
 
+import com.android.firstrssreader.data.RssItem;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class ListListener {
+public class ListListener implements OnItemClickListener {
 
+	List<RssItem> listItems;
+	Activity activity;
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
 }
